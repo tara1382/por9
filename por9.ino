a7a032b41754dcddea7b3fb519288736681beb7a
tara1382 <taragasemi00@gmail.com>
2024-10-14 16:15:33 -0700

    Add files via upload

---
+++ por9.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(float t = 0 ; t < 360 ; t++)
+{
+  float rad = (sin(t*PI/180));
+  Serial.print(rad);
+  delay(1);
+  
+  Serial.print("     ");
+
+  float rad1 = (sin(t*PI/180));
+  Serial.print(rad1);
+  delay(1);
+}
+}
